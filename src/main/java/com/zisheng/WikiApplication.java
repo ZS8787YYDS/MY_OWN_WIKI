498d61f52502646ac1fc97073fea607d26f38f92
zishengyyds <zisheng8787@gitee.com>
2023-10-31 01:30:16 +0800

    修改了启动日志及其图案

--- src/main/java/com/zisheng/WikiApplication.java
+++ src/main/java/com/zisheng/WikiApplication.java
@@ -1,13 +1,16 @@
 package com.zisheng;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class WikiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
+        log.info("SpringBoot项目启动成功！！！");
     }
 
 }
