f74df7311f90a772bc8bb4f0b60acad4a9a8a57c
zishengyyds <zisheng8787@gitee.com>
2023-11-05 23:38:43 +0800

    添加拦截器，打印接口耗时！！！

--- src/main/java/com/zisheng/Interceptors/TestInterceptor.java
+++ src/main/java/com/zisheng/Interceptors/TestInterceptor.java
@@ -12,19 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class TestInterceptor implements HandlerInterceptor {
     private static final Logger log = LoggerFactory.getLogger(TestInterceptor.class);
-    private static Long begin;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String url = request.getRequestURL().toString();
         log.info(request.getMethod() + ": " + url);
         log.info("目的主机地址：{}",request.getRemoteHost() + ":" + request.getRemotePort());
-        begin = System.currentTimeMillis();
+//        begin = System.currentTimeMillis();
+        request.setAttribute("executeStartTime",System.currentTimeMillis());
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // 存放开始时间
+        Long begin = (Long) request.getAttribute("executeStartTime");
         Long end = System.currentTimeMillis();
         log.info("接口执行了：{}秒",(end - begin) / 1000.0);
     }
